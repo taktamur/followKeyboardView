0560acf0ff59a25ea395730bf0002056644c5269
Takafumi Tamura <taktamur@gmail.com>
2015-08-24 12:33:52 +0900

    Initial Commit

---
+++ viewWithKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewWithKeyboard
+//
+//  Created by 田村孝文 on 2015/08/24.
+//  Copyright (c) 2015年 田村孝文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
